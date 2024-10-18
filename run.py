8a23ab068b316ec5a60c8d66082456298b4ac3b2
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2024-10-18 15:36:02 +0530

    updated

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import giskard
 
+import os
+
+from giskar.llm.client.mistral import MistralClient
+
+os.environ['MISTRAL_API_KEY'] = "wEqYDBG/+hKW/5/mIk8xEXUTrepTmbGdyVNJJY+l"
+
+mc = MistralClient()
+
+giskard.llm.set_default_client(mc)
 model_name = "microsoft/Phi-3-mini-4k-instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
