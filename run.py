ef378a4340617c9f737e4da54a53bb8693128097
Arun Suresh <58501088+asa1997@users.noreply.github.com>
2024-10-18 14:45:30 +0530

    updated

--- run.py
+++ run.py
@@ -1,49 +1,11 @@
-from langchain import OpenAI, FAISS, PromptTemplate
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.document_loaders import PyPDFLoader
-from langchain.chains import RetrievalQA
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import giskard
-import pandas as pd
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Prepare vector store (FAISS) with IPPC report
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, add_start_index=True)
-loader = PyPDFLoader("https://www.ipcc.ch/report/ar6/syr/downloads/report/IPCC_AR6_SYR_LongerReport.pdf")
-db = FAISS.from_documents(loader.load_and_split(text_splitter), OpenAIEmbeddings())
+model_name = "microsoft/Phi-3-mini-4k-instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
 
-# Prepare QA chain
-PROMPT_TEMPLATE = """You are the Climate Assistant, a helpful AI assistant made by Giskard.
-Your task is to answer common questions on climate change.
-You will be given a question and relevant excerpts from the IPCC Climate Change Synthesis Report (2023).
-Please provide short and clear answers based on the provided context. Be polite and helpful.
-
-Context:
-{context}
-
-Question:
-{question}
-
-Your answer:
-"""
-
-llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
-prompt = PromptTemplate(template=PROMPT_TEMPLATE, input_variables=["question", "context"])
-climate_qa_chain = RetrievalQA.from_llm(llm=llm, retriever=db.as_retriever(), prompt=prompt)
-
-
-
-def model_predict(df: pd.DataFrame):
-    """Wraps the LLM call in a simple Python function.
-
-    The function takes a pandas.DataFrame containing the input variables needed
-    by your model, and must return a list of the outputs (one for each row).
-    """
-    return [climate_qa_chain.run({"query": question}) for question in df["question"]]
-
-# Don’t forget to fill the `name` and `description`: they are used by Giskard
-# to generate domain-specific tests.
 giskard_model = giskard.Model(
-    model=model_predict,
+    model=model,
     model_type="text_generation",
     name="Climate Change Question Answering",
     description="This model answers any question about climate change based on IPCC reports",
@@ -51,5 +13,3 @@ giskard_model = giskard.Model(
 )
 
 scan_results = giskard.scan(giskard_model)
-
-print(scan_results)
